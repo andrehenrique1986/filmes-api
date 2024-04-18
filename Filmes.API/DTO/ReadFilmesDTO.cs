4e8a19f3028c7886748900db923abdeeb358921d
andre.silva <andre.silva@marimex.com.br>
2024-04-18 16:20:18 -0300

    Add project files.

---
+++ Filmes.API/DTO/ReadFilmesDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmes.API.DTO
+{
+    public class ReadFilmesDTO
+    {
+       
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public int Duracao { get; set; }
+        public DateTime HoraDaConsulta { get; set; } = DateTime.Now;
+    }
+}
---
+++ Filmes.API/Data/FilmeContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Filmes.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Filmes.API.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opts)
+            : base(opts)
+        {
+            
+        }
+
+        public DbSet<Models.Filmes> Filmes { get; set; }
+       
+    }
+}
